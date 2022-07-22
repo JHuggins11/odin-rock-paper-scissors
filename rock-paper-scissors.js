7395bc2ff2092163f55e930484ece0e7010805db
Jason <jasonhuggins11@outlook.com>
2022-07-22 19:22:11 +0100

    Add function to play a round of the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,3 +9,33 @@ function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
+
+// Plays a round of the game and returns the result
+function playRound(playerSelection, computerSelection) {
+    playerSelection = formatInput(playerSelection);
+
+    switch(true) {
+        // Player wins
+        case playerSelection === "Rock" && computerSelection === "Scissors":
+        case playerSelection === "Paper" && computerSelection === "Rock":
+        case playerSelection === "Scissors" && computerSelection === "Paper":
+            console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+            break;
+        // Computer wins
+        case playerSelection === "Rock" && computerSelection === "Paper":
+        case playerSelection === "Paper" && computerSelection === "Scissors":
+        case playerSelection === "Scissors" && computerSelection === "Rock":
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
+            break;
+        // Result is a draw
+        case playerSelection === "Rock" && computerSelection === "Rock":
+        case playerSelection === "Paper" && computerSelection === "Paper":
+        case playerSelection === "Scissors" && computerSelection === "Scissors":
+            console.log(`It's a draw! Both of you used ${computerSelection}!`);
+            break;
+        // Player wrote an invalid input as their choice
+        default:
+            console.log("Invalid choice. Please write either 'rock', 'paper' or 'scissors'.");
+            break;
+    }
+}
