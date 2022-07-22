8ec2ec0d7003f3436f6eabd82826a3c80a48d68c
Jason <jasonhuggins11@outlook.com>
2022-07-22 19:11:05 +0100

    Add function to format player's input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,9 @@
+// Format input with the first letter capitalised and the rest in lowercase (for the player's choice)
+function formatInput(text) {
+    let length = text.length;
+    return text.substr(0, 1).toUpperCase() + text.substr(1, length).toLowerCase();
+}
+
 // Randomly chooses an option for the opponent
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
