ba2e0618a83c96389f5f45c2061347a63e174a8e
Jason <jasonhuggins11@outlook.com>
2022-07-22 19:01:12 +0100

    Add function to randomly select computer choice

---
+++ rock-paper-scissors.js
@@ -0,0 +1,5 @@
+// Randomly chooses an option for the opponent
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+}
