ae8c6c4208d44915edd22730a914d0826511dd85
Jason <jasonhuggins11@outlook.com>
2022-07-28 19:00:14 +0100

    Change to forEach method

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,30 +6,34 @@ function getComputerChoice() {
 
 // Plays a round of the game
 function playRound(playerSelection, computerSelection) {
+    /* 
+        An integer indicating the result of a round (0 = no result, 1 = player wins, 2 = computer 
+        wins, 3 = draw)
+    */
+        let resultFlag = 0; 
+
     switch(true) {
         // Player wins
         case playerSelection === "Rock" && computerSelection === "Scissors":
         case playerSelection === "Paper" && computerSelection === "Rock":
         case playerSelection === "Scissors" && computerSelection === "Paper":
-            result.textContent = `You win! ${playerSelection} beats Computer's ${computerSelection}!`;
-            playerScore++;
+            resultFlag = 1;
             break;
         // Computer wins
         case playerSelection === "Rock" && computerSelection === "Paper":
         case playerSelection === "Paper" && computerSelection === "Scissors":
         case playerSelection === "Scissors" && computerSelection === "Rock":
-            result.textContent = `You lose! Computer used ${computerSelection} that beats ${playerSelection}!`;
-            computerScore++;
+            resultFlag = 2;
             break;
         // Result is a draw
         case playerSelection === "Rock" && computerSelection === "Rock":
         case playerSelection === "Paper" && computerSelection === "Paper":
         case playerSelection === "Scissors" && computerSelection === "Scissors":
-            result.textContent = `It's a draw! Both of you used ${computerSelection}!`;
+            resultFlag = 3;
             break;
     }
 
-    return;
+    return resultFlag;
 }
 
 const container = document.querySelector("#container");
@@ -50,20 +54,11 @@ const computerScoreText = document.createElement("p");
 computerScoreText.textContent = `Computer: ${computerScore}`;
 resultsSection.appendChild(computerScoreText);
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", playRound(buttons[i].value, getComputerChoice()));
-}
-
-if (playerScore === 5 && computerScore < 5) {
-    result.textContent = "You have won the game! Congratulations!";
-}
-else if (computerScore === 5 && playerScore < 5) {
-    result.textContent = "You have lost the game! Better luck next time!";
-}
-
-/* buttons.forEach(function (selectedButton) {
+buttons.forEach((selectedButton) => {
     selectedButton.addEventListener("click", () => {
-        let roundResult = playRound(selectedButton.value, getComputerChoice());
+        let playerSelection = selectedButton.textContent;
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
 
         switch (roundResult) {
             case 1: // Player wins round
@@ -74,12 +69,19 @@ else if (computerScore === 5 && playerScore < 5) {
                 result.textContent = `You lose! Computer used ${computerSelection} that beats ${playerSelection}!`;
                 computerScore++;
                 break;
-            case 3: // Draw
+            case 3: // Round ends in a draw
                 result.textContent = `It's a draw! Both of you used ${computerSelection}!`;
                 break;
         }
     });
-}); */
+});
+
+if (playerScore === 5 && computerScore < 5) {
+    result.textContent = "You have won the game! Congratulations!";
+}
+else if (computerScore === 5 && playerScore < 5) {
+    result.textContent = "You have lost the game! Better luck next time!";
+}
 
 /* let playerScore = 0;
 let computerScore = 0;
